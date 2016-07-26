34f084cf622daa486ef9e8cc505f24a2541101d3
themsg <michaelperng@gmail.com>
2016-07-26 15:39:17 -0500

    Fully fleshed-out edit movie form controller and unit tests

--- app/MovieEditFormCtrl.js
+++ app/MovieEditFormCtrl.js
@@ -1,20 +1,20 @@
 angular.module('moviesApp')
     .controller('MovieEditFormCtrl', ['$scope', '$localStorage', '$sessionStorage', '$routeParams',
     function ($scope, $localStorage, $sessionStorage, $routeParams) {
+        var id = parseInt($routeParams.movieId);
         var init = function() {
             $scope.$storage = $localStorage.$default({ movies: [] });
-            $scope.editMovie = findMovie();
+            $scope.editMovie = findMovie(id);
         }
 
-        var findMovie = function () {
-            return _.chain($scope.$storage.movies)
-                    .find({id:$routeParams.movieId});
+        var findMovie = function (id) {
+            return _.find($scope.$storage.movies, {'id':id});
         }
 
-        $scope.editMovie = function (invalid) {
+        $scope.updateMovie = function (invalid) {
             // add to movies data structure
             if(!invalid) {
-                findMovie().merge($scope.editMovie);
+                _.merge(findMovie(id), $scope.editMovie);
             }
         };
 
--- test/movies-spec.js
+++ test/movies-spec.js
@@ -114,3 +114,82 @@ describe('MovieFormCtrl', function() {
         expect(scope.$storage.movies.length).toEqual(0);
     });
 });
+
+describe('MovieEditFormCtrl', function() {
+    var scope, $location, createController, routeParams;
+    beforeEach(function() {
+        localStorage.clear();
+    });
+
+    beforeEach(module('moviesApp'));
+
+    beforeEach(inject(function ($rootScope, $controller, _$location_) {
+        $location = _$location_;
+        scope = $rootScope.$new();
+
+        createController = function() {
+            return $controller('MovieEditFormCtrl', {
+                '$scope': scope,
+                '$routeParams': routeParams
+            });
+        };
+    }));
+
+    it('should edit a movie', function() {
+        routeParams = {movieId: 0};
+        var controller = createController();
+        scope.$storage.movies = [
+            {
+                id: 0,
+                title: 'Test Movie',
+                genre: 'Test Genre',
+                actors: ['Test Actor1', 'Test Actor2'],
+                year: 1900,
+                rating: 10
+            }
+        ];
+        scope.editMovie = {
+            title: 'Test Movie2'
+        };
+        scope.updateMovie(false);
+        expect(scope.$storage.movies[0]);
+        expect(scope.$storage.movies[0].title).toEqual('Test Movie2');
+        expect(scope.$storage.movies[0].genre).toEqual('Test Genre');
+        expect(scope.$storage.movies[0].actors).toEqual(['Test Actor1', 'Test Actor2']);
+        expect(scope.$storage.movies[0].year).toEqual(1900);
+        expect(scope.$storage.movies[0].rating).toEqual(10);
+    });
+
+    it('should edit one movie out of multiple movies', function() {
+        routeParams = {movieId: 1};
+        var controller = createController();
+        scope.$storage.movies = [
+            {
+                id: 0,
+                title: 'Test Movie',
+                genre: 'Test Genre',
+                actors: ['Test Actor1', 'Test Actor2'],
+                year: 1900,
+                rating: 10
+            },
+            {
+                id: 1,
+                title: 'Test Movie 1',
+                genre: 'Test Genre 1',
+                actors: ['Test Actor1', 'Test Actor2'],
+                year: 1888,
+                rating: 0
+            }
+        ];
+        scope.editMovie = {
+            title: 'Test Movie2'
+        };
+        scope.updateMovie(false);
+        expect(scope.$storage.movies[1]);
+        expect(scope.$storage.movies[1].title).toEqual('Test Movie2');
+        expect(scope.$storage.movies[1].genre).toEqual('Test Genre 1');
+        expect(scope.$storage.movies[1].actors).toEqual(['Test Actor1', 'Test Actor2']);
+        expect(scope.$storage.movies[1].year).toEqual(1888);
+        expect(scope.$storage.movies[1].rating).toEqual(0);
+    });
+});
